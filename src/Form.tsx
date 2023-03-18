b6cb36c2665d419ef70fe7c360dd59a05ddf5dcc
nkumar007 <naveen.mbuff98@gmail.com>
2023-03-18 20:02:26 +0530

    using react-hook-form to build better forms

---
+++ src/Form.tsx
@@ -0,0 +1,42 @@
+import {} from "react";
+import { FieldValues, useForm } from "react-hook-form";
+
+const Form = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          id="name"
+          {...register("name")}
+          type="text"
+          className="form-control"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input
+          id="age"
+          {...register("age")}
+          type="number"
+          className="form-control"
+        />
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Form;
